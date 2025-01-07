ba1248d0a5532163c494e10e3cb87444aa6c28e7
Pranita Kute <97874560+PranitaKute@users.noreply.github.com>
2025-01-07 20:53:14 +0530

    Upload Spin Box Example

---
+++ spinBoxExample.py
@@ -0,0 +1,21 @@
+try:
+    from tkinter import *
+except ImportError:
+    from tkinter import *
+
+try:
+    import ttk 
+    py3 = False
+except ImportError:
+    import tkinter.ttk as ttk
+    py3 = True
+
+
+root = Tk() 
+
+numberBox = Spinbox(root, from_ = 0, to=100)
+numberBox.pack()
+
+
+root.title("SpinBox Example")
+root.mainloop()
